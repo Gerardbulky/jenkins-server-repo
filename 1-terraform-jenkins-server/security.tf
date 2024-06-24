5de0323b395519f37b2d5d783dd8730a5762a57e
Gerardbulky <gerardambe@yahoo.com>
2024-06-24 20:04:28 +0200

    added docker push script

--- 1-terraform-jenkins-server/security.tf
+++ 1-terraform-jenkins-server/security.tf
@@ -14,6 +14,13 @@ resource "aws_default_security_group" "default-sg" {
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
